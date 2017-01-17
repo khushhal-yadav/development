82a7a62dbc09190a8b54c5700f26ea62b759a581
khushhal-yadav <khushhal.s.yadav@gmail.com>
2017-01-17 01:07:18 +0000

    enhancements

--- WeatherTracker/src/main/resources/public/js/controllers/weatherCtrl.js
+++ WeatherTracker/src/main/resources/public/js/controllers/weatherCtrl.js
@@ -3,9 +3,10 @@ app.controller('weatherController', function($scope, $http, weatherService){
     $scope.location = '';
     var defaultLocation = 'Glasgow';
 
+    //function called at page loading
     $scope.initial = function(){
-        //TODO: show weather as per current location
-        /*if (navigator.geolocation) {
+        fetchData(defaultLocation);
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position){
                 latitude = position.coords.latitude;
                 longitude = position.coords.longitude;
@@ -13,30 +14,31 @@ app.controller('weatherController', function($scope, $http, weatherService){
                 console.log("currentLocation : " + currentLocation);
                 fetchData(currentLocation);
             });
-        } else {*/
-            fetchData(defaultLocation);
-        /*}*/
+        }
     };
 
+    // function called after entering location and hit enter
     $scope.refresh = function(){
         fetchData($scope.location);
     };
 
+    // function to fetch weather data from api call
     fetchData = function(location) {
         $('.loading').show();
-            url = weatherService.getUrl(location);
-            $http.get(url).
-                success(function(data){
-                    $scope.weatherData = data;
-                    angular.forEach($scope.weatherData.list, function(value1, i) {
+        $('.error').hide();
+        url = weatherService.getUrl(location);
+        $http.get(url).
+            success(function(data){
+                $scope.weatherData = data;
+                angular.forEach($scope.weatherData.list, function(value1, i) {
                         $scope.weatherData.list[i].date = weatherService.getDate(value1.dt_txt);
-                    });
-                $('.loading').hide();
-            	}).
-                error(function(){
-                     $('.loading').hide();
-                     $('.error').show().html("Some error occured while fetching weather details from API");
                 });
+                $('.loading').hide();
+            }).
+            error(function(){
+                $('.loading').hide();
+                $('.error').show().html("Some error occured while fetching weather details from API");
+            });
 
     }
 
--- WeatherTracker/src/main/resources/public/js/services/weatherService.js
+++ WeatherTracker/src/main/resources/public/js/services/weatherService.js
@@ -2,6 +2,7 @@ app.service('weatherService', function($http){
 
     var defaultLocation = 'Glasgow';
 
+    //function to get date string from date time string
     this.getDate = function(input) {
         if(input == undefined)
             return;
@@ -9,6 +10,7 @@ app.service('weatherService', function($http){
         return parts[0];
     };
 
+    // function to get url based on location
     this.getUrl = function(location) {
         return "http://api.openweathermap.org/data/2.5/forecast?q=" + location + "&mode=json&appid=c5075e558efec609f928da0f3ceb005f";
     };
